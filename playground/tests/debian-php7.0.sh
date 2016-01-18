03e53111c782ffe1972fbb2234fbe38409f0bd9b
Christian Stocker <me@chregu.tv>
2016-01-18 23:44:00 +0100

    add tests

---
+++ playground/tests/debian-php7.0.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+function finish() {
+    echo -e "\033[31mBuild failed, cleanup\e[0m\n- Stop vagrant"
+    cleanup
+    echo -e "\n\033[31mBuild NOT successfull\e[0m"
+    exit $1
+}
+
+function cleanup() {
+   vagrant destroy -f
+}
+
+trap finish EXIT SIGHUP SIGINT SIGTERM
+
+cd playground
+
+ln -s ../../tests/playbooks/debian-php7.0-parameters.yml  tests/playbooks/parameters.yml 
+
+export VIRTUALIZATION_PARAMETERS_FILE=tests/playbooks/debian-php7.0-parameters.yml
+
+export VAGRANT_DEFAULT_PROVIDER=$1
+
+
+vagrant destroy -f
+vagrant up
+
+trap - EXIT SIGHUP SIGINT SIGTERM
+
+cleanup
+
+if [[ -z $GITLAB_CI ]]; then
+    echo -e "\n\e[32mBuild successful\e[0m"
+fi
